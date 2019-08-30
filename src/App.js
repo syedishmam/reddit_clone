4c9496021724f528bfaa429796862866bbd3581a
NiazAhmed151 <niaz151@gmail.com>
2019-08-30 19:48:06 -0400

    Added File Structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App container border border-primary ">
+    <div className="App container border border-primary">
       test
     </div>
   );
