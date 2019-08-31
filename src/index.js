8915ea962c0429e2ff499792cba59d558979e6e7
Ish <syedishmam17@gmail.com>
2019-08-31 12:14:58 -0400

    Create store, compose enhcrs, nest App in Provider

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
+    );
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
