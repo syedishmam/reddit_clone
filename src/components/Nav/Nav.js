aabf81bff515d9c340619092cdefb4ca2a8dc696
niaz151 <niaz151@gmail.com>
2019-09-01 16:54:12 -0400

    Added Smaller User Action Logos

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 import DropDown from './DropDown';
-import Logo from './Logo';
 import UserActions from './UserActions';
+import {RedditLogo} from '../../logos';
 import '../../styles/NavContainer.css';
 
 class Nav extends React.Component {
     render() {
         return (
           <div className="nav-container pl-3 pr-3 border d-flex flex-row align-items-center justify-content-around">
-            <Logo />
+            <RedditLogo />
             <DropDown />
             <SearchBar />
             <UserActions />
--- src/components/Nav/UserActions.js
+++ src/components/Nav/UserActions.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import Logos from '../../logos';
+import {TrendingLogo,
+        AllLogo,
+        ChatLogo,
+        MessagesLogo,
+        CreatePostLogo} from '../../logos';
 import '../../styles/UserActions.css';
 
 class UserActions extends React.Component{
 
   render(){
     return(
-      <div className='border user-actions-container'>
-        User Actions
+      <div className='user-actions-container d-flex flex-row align-items-center justify-content-around'>
+        <TrendingLogo />
+        <AllLogo />
+        <ChatLogo />
+        <MessagesLogo />
+        <CreatePostLogo />
       </div>
     )
   }
