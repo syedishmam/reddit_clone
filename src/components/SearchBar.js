5f872afeed1796d368266333c25f0143c08ddba5
Ish <syedishmam17@gmail.com>
2019-08-31 23:59:02 -0400

    Add Enter key press logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React from 'react';
 import '../styles/SearchBarStyles.css';
 
 class SearchBar extends React.Component {
+    handleKeyDown(e) {
+        if(e.key === 'Enter') {
+            console.log('Entered');
+        }
+    }
+
     render() {
         return (
           <div className="border">
-            <input type="text" className="centered"/>
+            <input onKeyDown={this.handleKeyDown} type="text" className="search-bar my-1"/>
           </div>  
         );
     }
