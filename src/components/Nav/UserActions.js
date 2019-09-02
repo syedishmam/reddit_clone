ebcd43da1ee32ef2bf6bc1ce6f884588f6edff85
syedishmam <syedishmam17@gmail.com>
2019-09-02 15:01:10 -0400

    Comment out logos

--- src/components/Nav/UserActions.js
+++ src/components/Nav/UserActions.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
-import {TrendingLogo,
+/* import {TrendingLogo,
         AllLogo,
         ChatLogo,
         MessagesLogo,
         CreatePostLogo} from '../../logos';
+*/
 import '../../styles/UserActions.css';
 
 class UserActions extends React.Component{
 
   render(){
     return(
-      <div className='user-actions-container d-flex flex-row align-items-center justify-content-around'>
+      <div>
+
+      </div>
+      /*<div className='user-actions-container d-flex flex-row align-items-center justify-content-around'>
         <TrendingLogo />
         <AllLogo />
         <ChatLogo />
         <MessagesLogo />
         <CreatePostLogo />
-      </div>
+      </div> */
     )
   }
 }
