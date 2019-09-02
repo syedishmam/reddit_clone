2fc8f59c60d37282f53f6d107ca6b8e6cf153919
syedishmam <syedishmam17@gmail.com>
2019-09-02 15:19:04 -0400

    Uncomment imports and return statement

--- src/components/Nav/UserActions.js
+++ src/components/Nav/UserActions.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
-/* import {TrendingLogo,
+import {TrendingLogo,
         AllLogo,
         ChatLogo,
         MessagesLogo,
         CreatePostLogo} from '../../logos';
-*/
 import '../../styles/UserActions.css';
 
 class UserActions extends React.Component{
 
   render(){
     return(
-      <div>
-
-      </div>
-      /*<div className='user-actions-container d-flex flex-row align-items-center justify-content-around'>
+      <div className='user-actions-container d-flex flex-row align-items-center justify-content-around'>
         <TrendingLogo />
         <AllLogo />
         <ChatLogo />
         <MessagesLogo />
         <CreatePostLogo />
-      </div> */
+      </div> 
     )
   }
 }
